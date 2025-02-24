923c690c3af063a6d89326eb503230dfe5eb53f8
indahjann <jannahindah7@gmail.com>
2025-02-24 13:43:02 +0700

    Update MGaris.java

--- Praktikum_1/MGaris.java
+++ Praktikum_1/MGaris.java
@@ -14,14 +14,14 @@ public class MGaris {
 
         System.out.println("Jumlah Objek Garis = " + Garis.getCounterGaris());
 
-        System.out.println("Panjang Garis G1 adalah " + G1.panjangGaris());
-        System.out.println("Gradien Garis G1 adalah " + G1.gradienGaris());
+        System.out.println("Panjang Garis G1 adalah " + G1.getPanjangGaris());
+        System.out.println("Gradien Garis G1 adalah " + G1.getGradienGaris());
         System.out.print("Titik Tengah Garis G1 adalah ");
-        G1.tengahGaris().printTitik();
+        G1.getTengahGaris().printTitik();
 
         Garis G2 = new Garis(T1, T2);
-        System.out.println("Apakah G1 dan G2 sejajar? " + G1.cekSejajar(G2));
-        System.out.println("Apakah G1 dan G2 tegak lurus? " + G1.cekTegakLurus(G2));
+        System.out.println("Apakah G1 dan G2 sejajar? " + G1.IsSejajar(G2));
+        System.out.println("Apakah G1 dan G2 tegak lurus? " + G1.IsTegakLurus(G2));
 
         System.out.print("Persamaan Garis G1 : ");
         G1.printPersamaan();
